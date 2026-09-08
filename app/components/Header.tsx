556ff759c47864dd1745e36897cf6ec57990efaa
Nathan Garcia <n8g423@gmail.com>
2026-09-08 13:18:19 -0600

    Project overhaul beginning

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -13,24 +13,14 @@ export default function Header() {
           layoutId="site-signature"
           src="/signature.png"
           alt="Nathan Garcia"
-          className="w-full max-w-md md:max-w-lg h-auto mb-6 -ml-1"
+          className="w-full max-w-md md:max-w-lg h-auto mb-8 -ml-1"
         />
       )}
       <p className="max-w-2xl tracking-tight leading-relaxed text-[#666666]">
-        Nathan Garcia is a graphic designer based in Calgary, Alberta,
-        working across brand identity, event flyers, and merch design for
-        artists, festivals, and student organizations. Currently studying
-        Computer Information Systems at Mount Royal University, and
-        Communication Designer at{" "}
-        <a
-          href="https://www.mruhacks.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline text-black"
-        >
-          MRUHacks
-        </a>
-        .
+        is a graphic designer based in Calgary, Alberta,
+        working across brand identity, digital and physical assets for
+        artists, festivals, and organizations. Currently finishing their
+        Bachelor in Computer Information Systems at Mount Royal University.
       </p>
     </header>
   );
